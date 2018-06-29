14c8ae061c2cb53095379e47bdf94b981c01a47a
SlavaBiceps <slava_ivanov_cs_12321@mail.ru>
2018-06-29 12:32:34 +0500

    OK(codeforces)

--- B.pas
+++ B.pas
@@ -1,15 +1,17 @@
 var n:longint;
 s:string;
-procedure reverse2(s:string;l,r:longint);
+procedure reverse2(var s:string;l,r:longint);
 var s1:string;
 j:longint;
 begin
 j:=r;
+s1:=s;
 for var i:=l to r do
   begin
   s1[i]:=s[j];
   j-=1;
   end;
+s:=s1;
 end;
 begin
     readln(n);
@@ -17,7 +19,7 @@ begin
     for var i:=1 to n do
       begin
       if n mod i = 0 then
-            reverse2(s,1,i+1);
+            reverse2(s,1,i);
     end;
     writeln(s);
 end.
